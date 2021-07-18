dc94053630658424a2748b75299f0648ee54a376
Dana Alami <dana.alami18@gmail.com>
2021-07-18 22:00:04 +0300

    fixed ish images

--- groovy/src/components/pages/portfolio.js
+++ groovy/src/components/pages/portfolio.js
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
 import { Card, Col, Row , Button, Image} from "react-bootstrap"
+import scheduler from '../../images/scheduler.png'
+// import jsquiz from '../../images/jsquiz.png'
+import lookafteryourself3000 from '../../images/lookafteryourself3000.png'
+import yummydescription from '../../images/yummydescription.jpg'
 
 const Portfolio = () => {
     const [projects, setProject] = useState ([{name:"Work Scheduler", link:"https://danaalami-cloud.github.io/work-day-calendar-application/",
-    repolink: "https://github.com/Danaalami-cloud/work-day-calendar-application.git", Image:"./images/scheduler.png"    
+    repolink: "https://github.com/Danaalami-cloud/work-day-calendar-application.git", Image:"../images/scheduler.png"    
 }, {name:"Yummy fridge", link:"https://danaalami-cloud.github.io/Yummy_Fridge/",
 repolink: "https://github.com/Danaalami-cloud/Yummy_Fridge"},
 {name:"Lookaftetyourself3000", link:"https://lookafteryourself3000.herokuapp.com/",
 repolink: "https://github.com/Danaalami-cloud/NoteApp"},
 ])
+// let names = ['jsquiz', 'lookafteryourself3000', 'password', 'scheduler', 'yummydescription'];
+
+//         let Images = names.map(names => {
+
+//             <img
+//             src=require("./images/schedular.png").default
+//             alt = ""
+//                 className="img-responsive" />
+//         });
+
     return (
 <Row xs={1} md={2} className="g-4">
   {projects.map((project) => (
     <Col>
       <Card>
-        <Card.Img  as={ Image} variant="top" src="./images/schedular.png" alt={'calendar'} />
+        <Card.Img  as={ Image} variant="top" src={scheduler}  />
+        <Card.Img  as={ Image} variant="top" src={lookafteryourself3000} />
+        <Card.Img  as={ Image} variant="top" src={yummydescription}  />
         <Card.Body>
           <Card.Title>{project.name}</Card.Title>
           <Card.Text>
