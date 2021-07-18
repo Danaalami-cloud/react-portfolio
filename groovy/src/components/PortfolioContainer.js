610b78c33288d1aea4bc3decfd0e03ebccb3e0b9
Dana Alami <dana.alami18@gmail.com>
2021-07-18 13:27:01 +0300

    added code contents to src folders and deleted old folders

--- groovy/src/components/PortfolioContainer.js
+++ groovy/src/components/PortfolioContainer.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
-import About from './pages/About';
-import Blog from './pages/Blog';
-import Contact from './pages/Contact';
+import Home from './pages/home';
+import About from './pages/about';
+import Blog from './pages/portfolio';
+import Contact from './pages/contact';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
