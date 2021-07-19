0791df7ea1a1b1c750925281f9283bc7b72ef195
Dana Alami <dana.alami18@gmail.com>
2021-07-19 16:46:46 +0300

    added contents to home and about pages

--- groovy/src/components/pages/about.js
+++ groovy/src/components/pages/about.js
@@ -5,12 +5,11 @@ export default function About() {
     <div>
       <h1>About Page</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      Hello, I'm Dana Alami and I'm a gradute from the University of Manchester with an undegradute hons degree in Business and Management with Italian Language. 
+      I'm taking a coding course and diving into the web development world as I've been interested by it for some time and wanted to challenge myself with something new to learn.
+
+I was born and raised in Jerusalem, Palestine and went to Manchester to study. I spent a year abroad in Bergamo, Italy as part of my course to expand my knowledge in Italian history and culture. 
+I would love to get a job in Manchester and move there as I fell in love with the city and its people.
       </p>
     </div>
   );
--- groovy/src/components/pages/portfolio.js
+++ groovy/src/components/pages/portfolio.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import { Card, Col, Row , Button, Image} from "react-bootstrap"
 import scheduler from '../../images/scheduler.png'
-// import jsquiz from '../../images/jsquiz.png'
+ import jsquiz from '../../images/jsquiz.png'
 import lookafteryourself3000 from '../../images/lookafteryourself3000.png'
 import yummydescription from '../../images/yummydescription.jpg'
+import password from "../../images/password.png"
 
 const Portfolio = () => {
     const [projects, setProject] = useState ([{name:"Work Scheduler", link:"https://danaalami-cloud.github.io/work-day-calendar-application/",
-    repolink: "https://github.com/Danaalami-cloud/work-day-calendar-application.git", Image:"../images/scheduler.png"    
+    repolink: "https://github.com/Danaalami-cloud/work-day-calendar-application.git", Image1:require("../../images/scheduler.png").default,    
 }, {name:"Yummy fridge", link:"https://danaalami-cloud.github.io/Yummy_Fridge/",
-repolink: "https://github.com/Danaalami-cloud/Yummy_Fridge"},
+repolink: "https://github.com/Danaalami-cloud/Yummy_Fridge", Image2:require("../../images/yummydescription.jpg").default},
 {name:"Lookaftetyourself3000", link:"https://lookafteryourself3000.herokuapp.com/",
-repolink: "https://github.com/Danaalami-cloud/NoteApp"},
+repolink: "https://github.com/Danaalami-cloud/NoteApp", Image3:require("../../images/lookafteryourself3000.png").default},
+{name:"JavaScript Quiz", link:"https://danaalami-cloud.github.io/javascript-code-quiz/",
+repolink: "https://github.com/Danaalami-cloud/javascript-code-quiz", Image4:require("../../images/jsquiz.png").default},
+{name:"Password Generator", link:"https://danaalami-cloud.github.io/week-3-random-password-generator/",
+repolink: "https://github.com/Danaalami-cloud/week-3-random-password-generator", Image5:require("../../images/password.png").default},
 ])
 // let names = ['jsquiz', 'lookafteryourself3000', 'password', 'scheduler', 'yummydescription'];
 
@@ -28,10 +33,11 @@ repolink: "https://github.com/Danaalami-cloud/NoteApp"},
   {projects.map((project) => (
     <Col>
       <Card>
-        <Card.Img  as={ Image} variant="top" src={scheduler} 
-        if (scheduler) />
-        <Card.Img  as={ Image} variant="top" src={lookafteryourself3000} />
-        <Card.Img  as={ Image} variant="top" src={yummydescription}  />
+        <Card.Img  src={ project.Image1 } variant="top"   />
+        <Card.Img  src={ project.Image2} variant="top"  />
+        <Card.Img  src={ project.Image3} variant="top"   />
+        <Card.Img  src={ project.Image4} variant="top"   />
+        <Card.Img  src={ project.Image5} variant="top"   />
         <Card.Body>
           <Card.Title>{project.name}</Card.Title>
           <Card.Text>
