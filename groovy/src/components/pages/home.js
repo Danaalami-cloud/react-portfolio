e2c68e9167901fc09d960ea74ef85376c1750e2f
Dana Alami <dana.alami18@gmail.com>
2021-07-19 21:29:12 +0300

    trying to add icons

--- groovy/src/components/pages/home.js
+++ groovy/src/components/pages/home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 // import '../src/App.css';
+import '../../images/github-logo.png'
 export default function Home() {
   return (
+    
     <div className='home'>
       <h1>Home Page</h1>
       <p>
@@ -10,12 +12,15 @@ export default function Home() {
         Linked, email or Github.
         Looking forward to become freinds or a future employee! :)
       </p>
-         
+      <a href="https://www.linkedin.com/in/dana-alami-2b6b2815a/"   rel="noopener noreferrer"  target="_blank" >    
+    <img src="https://github.com/DwinaTech/public-images/blob/main/linkedin-icon.png?raw=true"  alt="LinkIn-icon"/>  
+     </a>
+     <a href="https://github.com/Danaalami-cloud"   rel="noopener noreferrer"  target="_blank" >    
+    <img src="../../images/github-logo.png"  alt="Github-icon" /> 
+     </a>
     </div>
     
-    <a href="https://www.linkedin.com/in/lloret82/"   target="_blank" >        
-    <img src="https://github.com/DwinaTech/public-images/blob/main/linkedin-icon.png?raw=true"  alt="LinkIn-icon />  
-     </a>
+    
     
 
     
