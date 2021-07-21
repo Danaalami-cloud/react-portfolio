c447225998c221ec2ca6f441623a8b6e95ff0c1c
Dana Alami <dana.alami18@gmail.com>
2021-07-21 13:38:42 +0300

    moved groovy contents into the root

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/App.css';
+import './App.css';
 
 function App () {
   return (
